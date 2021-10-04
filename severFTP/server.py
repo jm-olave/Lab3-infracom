0eb4fb66c45a7dc7624d47cb81aef95c57a9d7ab
Santiago <s.morenom@uniandes.edu.co>
2021-10-04 11:32:33 -0500

    corrección

--- severFTP/server.py
+++ severFTP/server.py
@@ -4,27 +4,28 @@ import threading
 
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 4456
-ADDR = (IP, PORT)
+ADDRESS = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 SERVER_DATA_PATH = "server_data"
 
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected")
+def handle_client(conn, add):
+    print(f"[NEW CONNECTION] {add} connected")
     conn.send("OK@Welcome to the Server".encode(FORMAT))
 
 
 def main():
     print("The server is starting")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
+    server.bind(ADDRESS)
     server.listen()
     print("The server is listening")
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        conn, add = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, add))
         thread.start()
 
-    if __name__ == "__main__":
-        main()
+
+if __name__ == "__main__":
+    main()
