02faea5f20f54d41e4e906b6721252362a84fa97
Santiago <s.morenom@uniandes.edu.co>
2021-10-04 11:09:17 -0500

    Server.py

--- severFTP/server.py
+++ severFTP/server.py
@@ -1,37 +1,30 @@
 import socket
+import os
+import threading
+
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 4456
 ADDR = (IP, PORT)
-FORMAT = "utf-8"
 SIZE = 1024
+FORMAT = "utf-8"
+SERVER_DATA_PATH = "server_data"
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    conn.send("OK@Welcome to the Server".encode(FORMAT))
+
+
 def main():
-    #SOCKET_STREAM significa usar el modelo de tcp para la conexion
-    print("[STARTING]  Server is starting")
+    print("The server is starting")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
     server.listen()
-    print("[LISTENING]  Server is listening")
-
+    print("The server is listening")
     while True:
         conn, addr = server.accept()
-        print(f"[NEW CONNECTION] {addr} connected ")
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
 
-        filename = conn.recv(SIZE).decode(FORMAT)
-        #recibir el archivo desde el cliente
-        print("[RECV]"+filename+"FILENAME RECEIVED")
-        file = open("server_data/"+filename, "w")
-        conn.send("Filename received".encode(FORMAT))
-        #verifica contenido de los datos 
-        data = conn.recv(SIZE).decode(FORMAT)
-        print(f"[RECV] file data received" )
-        file.write(data)
-        conn.send("File data received".encode(FORMAT))
-
-        file.close()
-        conn.close()
-        print(f"[DISCONNECTED] {addr} disconnnected") 
-
-
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
