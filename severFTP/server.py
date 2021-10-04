281108a0c7dd83a4b6f52404a709725eb30c8f76
Santiago <s.morenom@uniandes.edu.co>
2021-10-04 12:15:33 -0500

    Server.py

--- severFTP/server.py
+++ severFTP/server.py
@@ -11,8 +11,63 @@ SERVER_DATA_PATH = "server_data"
 
 
 def handle_client(conn, add):
-    print(f"[NEW CONNECTION] {add} connected")
-    conn.send("OK@Welcome to the Server".encode(FORMAT))
+    print(f"[NEW CONNECTION] {add} conectado")
+    conn.send("OK@Bienvenido al servidor".encode(FORMAT))
+    while True:
+        data = conn.recv(SIZE).decode(FORMAT)
+        data = data.split("@")
+        data = data.split("@")
+        cmd = data[0]
+
+        if cmd == "LIST":
+            files = os.listdir(SERVER_DATA_PATH)
+            send_data = "OK@"
+
+            if len(files) == 0:
+                send_data += "El server esta vacio"
+            else:
+                send_data += "\n".join(f for f in files)
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "UPLOAD":
+            name, text = data[1], data[2]
+            filepath = os.path.join(SERVER_DATA_PATH, name)
+            with open(filepath, "w") as f:
+                f.write(text)
+
+            send_data = "OK@UPLOAD exitosa."
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "DELETE":
+            files = os.listdir(SERVER_DATA_PATH)
+            send_data = "OK@"
+            filename = data[1]
+
+            if len(files) == 0:
+                send_data += "El directorio del servidor esta vacio"
+            else:
+                if filename in files:
+                    os.system(f"rm {SERVER_DATA_PATH}/{filename}")
+                    send_data += "Archivo eliminado."
+                else:
+                    send_data += "No se encontro el archivo."
+
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "LOGOUT":
+            break
+        elif cmd == "HELP":
+            data = "OK@"
+            data += "LIST: Enlistar todos los archivos del servidor.\n"
+            data += "UPLOAD <path>: Subir un archivo al servidor.\n"
+            data += "DELETE <filename>: Eliminar un archivo del servidor.\n"
+            data += "LOGOUT: Desconectarse del servidor.\n"
+            data += "HELP: Enlistar los comandos."
+
+            conn.send(data.encode(FORMAT))
+
+    print(f"[DISCONNECTED] {add} desconectado")
+    conn.close()
 
 
 def main():
