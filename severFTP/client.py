8ebba4fc8e748e1e51ea61f8ee9623be03be8aff
jm-olave <jm.olave@uniandes.edu.co>
2021-10-04 11:07:23 -0500

    Update client.py
    client changed

--- severFTP/client.py
+++ severFTP/client.py
@@ -5,6 +5,7 @@ PORT = 4456
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
+SIZE = 1024
 
 
 def main():
@@ -12,22 +13,41 @@ def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
     
-    file = open("data/test.txt", "r")
-    data = file.read()
+    while True:
+        data = client.recv(SIZE).decode(FORMAT)
+        cmd, msg = data.split("@")
 
-    client.send("test.txt".encode(FORMAT))
-    msg = client.recv(SIZE).decode(FORMAT)
+        if cmd == "DISCONNECTED":
+            print(f"[SERVER]: {msg}")
+            break
+        elif cmd == "OK":
+            print(f"{msg}")
 
-    print(f"[LISTENING]: {msg}  Server is listening")
+        data = input("> ")
+        data = data.split(" ")
+        cmd = data[0]
 
-    client.send(data.encode(FORMAT))
-    msg = client.recv(SIZE).decode(FORMAT)
-    print(f"[SERVER]: {msg}")
-    #cerrar conexion
-    file.close()
+        if cmd == "HELP":
+            client.send(cmd.encode(FORMAT))
+        elif cmd == "LOGOUT":
+            client.send(cmd.encode(FORMAT))
+            break
+        elif cmd == "LIST":
+            client.send(cmd.encode(FORMAT))
+        elif cmd == "DELETE":
+            client.send(f"{cmd}@{data[1]}".encode(FORMAT))
+        elif cmd == "UPLOAD":
+            path = data[1]
+
+            with open(f"{path}", "r") as f:
+                text = f.read()
+
+            filename = path.split("/")[-1]
+            send_data = f"{cmd}@{filename}@{text}"
+            client.send(send_data.encode(FORMAT))
+
+    print("Disconnected from the server.")
     client.close()
 
-
-
 if __name__ == "__main__":
-    main()   
+    main()
